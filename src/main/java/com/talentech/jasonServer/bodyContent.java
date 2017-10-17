470e0337d6d3cac159f1f6849c75f088bd3adb97
TalenTech <italentech@gmail.com>
2017-10-17 12:49:32 -0400

    adding new java class for body content

---
+++ src/main/java/com/talentech/jasonServer/bodyContent.java
@@ -0,0 +1,35 @@
+package com.talentech.jasonServer;
+
+public class bodyContent {
+	private String id;
+	private String title;
+	private String author;
+	
+	public String getId(){
+		return id;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+		
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+		
+	}
+	
+	public String getAuthod(){
+		return author;
+	}
+	
+	public void setAuthor(String author){
+		this.author = author;
+		
+	}
+
+}
